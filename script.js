fd67c40a281d20b850474990de9565b21237bd4d
Talent M <simeontofiakwa@gmail.com>
2021-10-30 15:21:00 +0100

    adding and deleting objects

--- script.js
+++ script.js
@@ -56,6 +56,14 @@ console.log(me);
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
+me.skills = {
+    Javascript:true,
+    Typescript:false,
+    Python:true,
+    React:true,
+    Php:false
+}
+console.log(me);
 
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
