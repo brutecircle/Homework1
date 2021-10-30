6accaaded1e0a093443f4d7b54e3d287cadf4b46
Talent M <simeontofiakwa@gmail.com>
2021-10-30 14:35:23 +0100

    3

--- script.js
+++ script.js
@@ -14,14 +14,21 @@ ASSIGNMENT RULES
 /* EXERCISE A
    Create a variable called test and assign a string value to it.
 */
+let test = "Hello";
 
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
+let sum = 10 + 20;
+console.log(sum)
 
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
+
+// /math.floor rounds up numbers/
+let random = Math.floor(Math.random() * 20) + 0
+console.log(random);
 
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
